b83c7257ae5b0bb231471f35d79892f4f1def50c
laitrangg <tranglkhe176485@fpt.edu.vn>
2023-03-22 23:25:57 +0700

    Edit Login

--- MyPRJ301Project/src/java/controller/auth/LoginController.java
+++ MyPRJ301Project/src/java/controller/auth/LoginController.java
@@ -5,13 +5,16 @@
 package controller.auth;
 
 import dal.AccountDBContext;
+import dal.GroupDBContext;
 import java.io.IOException;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 import model.Account;
-import model.Role;
+import model.Group;
 
 /**
  *
@@ -22,6 +25,17 @@ public class LoginController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        Cookie[] listCookie = request.getCookies();
+        if (listCookie != null) {
+            for (Cookie c : listCookie) {
+                if (c.getName().equals("u")) {
+                    request.setAttribute("username", c.getValue());
+                }
+                if (c.getName().equals("p")) {
+                    request.setAttribute("password", c.getValue());
+                }
+            }
+        }
         request.getRequestDispatcher("view/auth/login.jsp").forward(request, response);
     }
 
@@ -40,20 +54,31 @@ public class LoginController extends HttpServlet {
         String password = request.getParameter("password");
         AccountDBContext db = new AccountDBContext();
         Account account = db.get(username, password);
+        GroupDBContext gdb = new GroupDBContext();
+        ArrayList<Group> groups = gdb.getSubject(username);
         if (account == null) {
             response.sendRedirect("login");
         } else {
+            Cookie cookieUser = new Cookie("u", username);
+            Cookie cookiePassword = new Cookie("p", password);
+            cookieUser.setMaxAge(60 * 60 * 24);
+
+            response.addCookie(cookieUser);
+            response.addCookie(cookiePassword);
+            request.setAttribute("groups", groups);
             request.getSession().setAttribute("account", account);
-            Role role = account.getRoles().get(0);
-            if (role != null) {
-                if (role.getId() == 2) {
-                    request.getRequestDispatcher("/view/student/home.jsp").forward(request, response);
-                } else {
-                    request.getRequestDispatcher("/view/lecturer/home.jsp").forward(request, response);
-                }
-            } else {
-                response.getWriter().println("This account doesn't have any role");
-            }
+            request.getRequestDispatcher("/view/home.jsp").forward(request, response);
+            
         }
     }
+    
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
 }
